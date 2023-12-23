0b1d7c1cfc5af7194190e7f4db7754349599c326
bomingeem <bomingeem@gmail.com>
2023-12-23 21:18:16 +0900

    feat: 코틀린에서 연산자를 다루는 방법

--- src/main/kotlin/lec01/Null.kt
+++ src/main/kotlin/lec01/Null.kt
@@ -20,7 +20,7 @@ class Null {
          * Kotlin이 null 관련 정보를 알 수 없는 타입
          * Runtime 시 Exception이 발생할 수 있다
          */
-        val person = Person("공부하는 개발자")
+        val person = Person("공부하는 개발자", 29)
         startsWithA(person.name)
     }
 
---
+++ src/main/kotlin/lec01/Operator.kt
@@ -0,0 +1,20 @@
+package lec01
+
+class Operator {
+    fun main() {
+        val money1 = JavaMoney(1_000L)
+        val money2 = money1
+        val money3 = JavaMoney(1_000L)
+
+        if (money1 > money2) {
+            println("Money1이 Money2보다 금액이 큽니다")
+        }
+
+        println(money1 === money2)
+        println(money1 == money3)
+
+        val money4 = Money(1_000L)
+        val money5 = Money(2_000L)
+        println(money4 + money5)
+    }
+}
