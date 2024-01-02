536d98e8f223c945a813b2d3cd348022d7c36c7d
bomingeem <bomingeem@gmail.com>
2024-01-02 23:19:43 +0900

    feat: 코틀린에서 예외를 다루는 방법

---
+++ src/main/kotlin/lec02/Exception.kt
@@ -0,0 +1,40 @@
+package lec02
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+
+class Exception {
+    fun parseIntOrThrow(str: String): Int {
+        try {
+            return str.toInt()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("주어진 ${str}은 숫자가 아닙니다")
+        }
+    }
+
+    fun parseIntOrThrowV2(str: String): Int? {
+        return try {
+            str.toInt()
+        } catch (e: NumberFormatException) {
+            null
+        }
+    }
+
+    /**
+     * Kotlin에서 모든 예외는 Unchecked Exception이다
+     */
+    fun readFile() {
+        val currentFile = File(".")
+        val file = File(currentFile.absolutePath + "/a.txt")
+        val reader = BufferedReader(FileReader(file))
+        println(reader.readLine())
+        reader.close()
+    }
+
+    fun readFile(path: String) {
+        BufferedReader(FileReader(path)).use { reader ->
+            println(reader.readLine())
+        }
+    }
+}
